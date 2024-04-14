49cc08a4a92102b6d4117b960ff21749ad8fd5c0
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-04-14 16:45:43 +0600

    update

---
+++ PHP1/Practice_Php/from_connect_mysql.php
@@ -0,0 +1,39 @@
+<?php
+   if(isset($_POST['']))
+
+?>
+
+
+
+
+
+
+
+<head> 
+    <body>
+        <form action="#" method="post" border="1"> 
+            <label for="name"> Full Name
+                <input type="text" name="name">
+            </label><br><br>
+
+            <label for="address">Address 
+                <input type="text" name="address">
+            </label><br><br>
+
+            <label for="email">Email 
+                <input type="emial" name="email">
+            </label><br><br>
+
+            <label for="select">Gender 
+                <select name="select"> 
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="other">Other</option>
+                </select>
+            </label><br><br>
+
+            <input type="submit" name="btn" value="submit">
+
+        </form>
+    </body>
+</head>
