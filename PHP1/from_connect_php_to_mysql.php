ad319d8b2a4c4ec0658bf0ef713ba7d3dcb2e5c5
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-04-16 22:06:29 +0600

    update

--- PHP1/from_connect_php_to_mysql.php
+++ PHP1/from_connect_php_to_mysql.php
@@ -1,11 +1,28 @@
+<?php
 
+    $conn = new mysqli('localhost','','root','information');
+    if(isset($_POST['btn'])){ 
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        
+    }
+
+?>
 
 
 
 <head> 
     <body>
         <form action="#" method="POST"> 
-            
+            <label for="name">Name 
+                <input type="text" name="name" placeholder="enter your name">
+            </label><br><br>
+
+            <label for="email">Email 
+                <input type="email" name="email" placeholder="enter your email">
+            </label><br><br>
+
+            <input type="submit" name="btn" value="submit">
         </form>
     </body>
 </head>
