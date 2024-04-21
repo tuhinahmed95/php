73f07b13459c1800da0040ae14a3f754a6194dc6
Hassan Mahfuj <humahfuj@gmail.com>
2024-04-21 15:16:44 +0600

    up

--- PHP1/php_project/navbar.php
+++ PHP1/php_project/navbar.php
@@ -4,13 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
     <div class="container"> 
+
         <div class="logo"> 
-            <img src="" alt="">
+            <img src="logo.png" alt="#">
         </div>
+
+        <div class="main-menu"> 
+            <ul> 
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Contact</a>
+                <a href="#">Service</a>
+            </ul>
+        </div>
+
+        <div class="end-nav"> 
+          Admin Login  <input type="login"><br><br>
+          User Login  <input type="login">
+        </div>
+
     </div>
     
 </body>
