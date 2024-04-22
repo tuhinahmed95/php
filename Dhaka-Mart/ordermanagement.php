74a4ea3bb2b49ec1becca5f4f05487e79b3533da
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-04-22 10:59:11 +0600

    up

---
+++ Dhaka-Mart/ordermanagement.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container"> 
+        <div class="row"> 
+            <div class="left-side"> 
+                <h3>Recent Activity</h3>
+            </div>
+
+           <div class="main-body"> 
+
+           <div class="total-customer"> 
+                <h4>Total Customer 
+                    <br>
+                    3205
+                </h4>
+            </div>
+
+            <div class="visitors"> 
+                <h4>Visitors 
+                    <br>
+                    1237
+                </h4>
+            </div>
+
+
+           </div>
+
+
+        </div>
+    </div>
+    
+</body>
+</html>
