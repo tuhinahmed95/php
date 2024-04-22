15eb948037ff6c19cd4346d68baa5c3f1dd5a2e3
Hassan Mahfuj <humahfuj@gmail.com>
2024-04-22 18:41:51 +0600

    table

--- PHP1/Dhaka-Mart/salesmanagement.php
+++ PHP1/Dhaka-Mart/salesmanagement.php
@@ -51,8 +51,28 @@
 
     </div>
 
-    <div class="table"> 
-        <table> 
+    
+
+    <!-- this is a form -->
+
+    <div class="form"> 
+    <h3>SALES + NEW SALE</h3>
+
+        <input type="text" name="bill" placeholder="Bill Number">
+        <input type="text" name="start-date" placeholder="Start Date">
+        <input type="text" name="end-date" placeholder="End Date">
+        <input type="text" name="select-customer" placeholder="Select Customer"><br><br>
+        <input type="text" name="select-product" placeholder="Select Product"><br><br>
+        <input type="submit" name="filter" value="filter">
+        <input type="reset" name="reset" value="reset">
+
+
+    </div>
+
+
+    <!-- this is table -->
+    <div> 
+        <table class="table1"> 
             <tr> 
                 <th>Sl No</th>
                 <th>Invoice No</th>
@@ -69,6 +89,58 @@
                 <th>Status</th>
                 <th>Action</th>
             </tr>
+
+            <tr> 
+                <td>01</td>
+                <td>312</td>
+                <td>Walk-in Customer</td>
+                <td>12 v 7.5ah code 00000560 + pc</td>
+                <td>22 Apr 2024</td>
+                <td>0 %</td>
+                <td>8001 Tk</td>
+                <td>0.00 Tk</td>
+                <td>0 Tk</td>
+                <td>0 Tk</td>
+                <td>7001 Tk</td>
+                <td>50</td>
+                <td>Paid</td>
+                
+            </tr>
+
+            <tr> 
+                <td>02</td>
+                <td>313</td>
+                <td>Walk-in Customer</td>
+                <td>12 v 7.5ah code 00000160 + fc</td>
+                <td>22 Apr 2024</td>
+                <td>0 %</td>
+                <td>8001 Tk</td>
+                <td>0.00 Tk</td>
+                <td>0 Tk</td>
+                <td>0 Tk</td>
+                <td>7001 Tk</td>
+                <td>50</td>
+                <td>Paid</td>
+                
+            </tr>
+
+            <tr> 
+                <td>03</td>
+                <td>314</td>
+                <td>Walk-in Customer</td>
+                <td>12 v 7.5ah code 00000160 + ffc</td>
+                <td>22 Apr 2024</td>
+                <td>0 %</td>
+                <td>8001 Tk</td>
+                <td>0.00 Tk</td>
+                <td>0 Tk</td>
+                <td>0 Tk</td>
+                <td>7001 Tk</td>
+                <td>50</td>
+                <td>Paid</td>
+                
+            </tr>
+
         </table>
     </div>
 
