fc73024fc450223e626fda9f104381500d99b899
Hassan Mahfuj <humahfuj@gmail.com>
2024-04-22 16:14:40 +0600

    up

---
+++ PHP1/Dhaka-Mart/salesmanagement.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+
+<div class="conatainer"> 
+    <h2>Sale</h2>   
+    <div class="navbar"> 
+        
+
+        <div class="first"> 
+            <h3>Sold Today:</h3>
+        </div>
+
+        <div class="second"> 
+            <h3>0Tk</h3>
+        </div>
+
+        <div class="third"> 
+            <h3>Today Received:</h3>
+        </div>
+
+        <div class="four"> 
+            <h3>0Tk</h3>
+        </div>
+
+    </div>
+
+</div>
+    
+</body>
+</html>
+
+
