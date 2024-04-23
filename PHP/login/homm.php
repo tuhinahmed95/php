b4a0566a7f9f21b380ff8094988a786f04b7f0b0
Hassan Mahfuj <humahfuj@gmail.com>
2024-04-23 17:35:39 +0600

    up

--- PHP/login/homm.php
+++ PHP/login/homm.php
@@ -4,8 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>this is from</h1>
+
+
+    <center> 
+        <form class="homfrm" action="#" method="POST"> 
+            Id <br>
+            <input type="text" name="id"><br><br>
+            Name <br>
+            <input type="text" name="name"><br><br>
+            Addrss <br>
+            <input type="text" name="address"><br><br>
+            <input type="submit" name="logout" value="Submit">
+            <button><a href="logout.php">Logout</a></button>
+        </form>
+    </center>
+
+
+    
+<?php
+
+session_start();
+
+if(!isset($_SESSION['syntax'])){ 
+    header("location:login.php");
+}
+
+
+?>
+
+   
 </body>
 </html>
--- PHP/login/login.php
+++ PHP/login/login.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <title>login</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
@@ -13,15 +14,24 @@
             $username   = $_POST['username'];
             $password   = $_POST['pass'];
 
-            
+            if($username == "syntax" && $password == "12345"){ 
+                $_SESSION['syntax']=$username;
+                header("location:homm.php");
+            }
+            else{ 
+                $message = "Username And Password Is Not Correct";
+            }
         }
 
+        ?>
 
+        <?php
+        echo isset($message)?$message:"";
 
         ?>
 
         <center> 
-            <form action="homm.php" method="POST"> 
+            <form class="loginform" action="#" method="POST"> 
                 Username <br>
                 <input type="text" name="username" required=""><br><br>
                 Password <br>
