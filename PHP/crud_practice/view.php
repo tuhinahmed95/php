e958488113763b8828f02d3589255cf3c0d11dea
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-05-16 23:57:14 +0600

    crud

--- PHP/crud_practice/view.php
+++ PHP/crud_practice/view.php
@@ -9,6 +9,8 @@ $conn = mysqli_connect("localhost","root","","php_db");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Crud</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
 </head>
 <body>
 
