af4b7fea2e46a8b9c1133ac1edc7cd6255810e6d
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-05-16 11:32:30 +0600

    crud

---
+++ PHP/crud_practice/insert.php
@@ -0,0 +1,31 @@
+
+<?php
+
+
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crud Operation</title>
+</head>
+<body>
+
+    <form action="" method="POST"> 
+        Firstname : <br>
+        <input type="text" name="firstname"><br><br>
+        Lastname : <br>
+        <input type="text" name="lastname"><br><br>
+        Email : <br>
+        <input type="text" name="email"><br><br>
+        <input type="submit" name="submit" value="submit">
+    </form>
+    
+</body>
+</html>
