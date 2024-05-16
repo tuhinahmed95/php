1a699f4a75a2ae78249619204f71f165cae7e847
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-05-16 23:31:19 +0600

    crud

--- PHP/crud_practice/view.php
+++ PHP/crud_practice/view.php
@@ -2,3 +2,34 @@
 $conn = mysqli_connect("localhost","root","","php_db");
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crud</title>
+</head>
+<body>
+
+    <div class="container"> 
+        <div class="row"> 
+            <div class="col-sm-2"></div>
+            <div class="col-sm-8"> 
+
+            <?php
+
+            $sql = "SELECT * FROM student";
+            $query = mysqli_query($conn,$sql);
+            
+
+
+            ?>
+
+            </div>
+            <div class="col-sm-2"></div>
+        </div>
+    </div>
+    
+</body>
+</html>
