04651745e5c35e72a12cddaed0feda8552ddc7ea
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-05-23 20:59:11 +0600

    crud

---
+++ PHP1/Practice_C/insert.php
@@ -0,0 +1,54 @@
+<?php
+$conn = mysqli_connect("localhost","root","","crud_practice");
+if(isset($_POST['submit'])){ 
+    $name    = $_POST['name'];
+    $email   = $_POST['email'];
+    $address = $_POST['address'];
+
+    $sql    = "INSERT INTO information(name,email,address) VALUES('$name','$email','$address')";
+    if(mysqli_query($conn,$sql) == TRUE){ 
+        echo "data inserted successfully";
+        header("location:view.php");
+
+    }else{ 
+        echo "data is not inserted";
+    }
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+
+    <div class="container"> 
+        <div class="row"> 
+            <div class="col-sm-3"></div>
+
+            <div class="col-sm-6 mt-4 pt-4 border border-success"> 
+            <h3 class="bg-success text-center">Registration Form</h3>
+
+                <form action="insert.php" method="POST"> 
+                    Name : <br>
+                    <input type="text" name="name"><br><br>
+                    E-Mail : <br>
+                    <input type="text" name="email"><br><br>
+                    Address : <br>
+                    <input type="text" name="address"><br><br>
+                    <input type="submit" name="submit" value="Insert" class="btn btn-success">
+                </form>
+
+            </div>
+
+            <div class="col-sm-3"></div>
+        </div>
+    </div>
+    
+</body>
+</html>
