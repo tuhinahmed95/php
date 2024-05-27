19c1fcd3354d198c26d3d900c69b9287aba4a709
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-05-27 11:16:53 +0600

    file

--- PHP/File Upload/insert.php
+++ PHP/File Upload/insert.php
@@ -14,7 +14,12 @@
         <div class="row"> 
             <div class="col-sm-3"></div>
             <div class="col-sm-6"> 
-                <form action=""></form>
+                <form action="#" method="POST" enctype=""> 
+                    Name : <br>
+                    <input type="text" name="name"><br><br>
+                    Image : <br>
+                    <input type="file" name="image"><br><br>
+                </form>
             </div>
             <div class="col-sm-3"></div>
         </div>
