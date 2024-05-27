d40097358785dbadca7a5e1700e7504ddb8fd9ad
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-05-27 11:14:15 +0600

    file

---
+++ PHP/File Upload/insert.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container"> 
+        <div class="row"> 
+            <div class="col-sm-3"></div>
+            <div class="col-sm-6"> 
+                <form action=""></form>
+            </div>
+            <div class="col-sm-3"></div>
+        </div>
+    </div>
+</body>
+</html>
