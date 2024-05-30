9bcff1ec967163534407a6a97251b2a99f9f161d
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-05-31 01:22:23 +0600

    file

--- PHP/File Upload/insert.php
+++ PHP/File Upload/insert.php
@@ -15,6 +15,15 @@
         <div class="row"> 
             <div class="col-sm-3"></div>
             <div class="col-sm-6"> 
+                <form action="insert.php"> 
+                    Product Name : <br>
+                    <input type="text" name="name"><br><br>
+                    Product Category : <br>
+                    <input type="text" name="pname"><br><br>
+                    Product Price : <br>
+                    <input type="text" name="price"><br><br>
+                    
+                </form>
 
             </div>
             <div class="col-sm-3"></div>
