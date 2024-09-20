8d2dc47b63632cbfa66c7c003313f9431457fa14
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-09-20 17:04:17 +0600

    crud php

---
+++ New_Crud_Operation/edit.php
@@ -0,0 +1,56 @@
+<?php
+$conn = mysqli_connect('localhost','root','','testdb');
+
+if($_GET['id']){ 
+    $getid = $_GET['id'];
+
+    $sql = "SELECT * FROM student WHERE id= $getid";
+
+    $query = mysqli_query($conn, $sql);
+
+    $data = mysqli_fetch_assoc($query);
+
+    $id = $data['id'];
+    $firstname = $data['firstname'];
+    $lastname = $data['lastname'];
+    $email = $data['email'];
+}
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+                <div class="col-sm-3"></div>
+            
+            <div class="col-sm-6 pt-5 border border-success mt-3">
+                <h3 class="bg-success">Registration Form</h3>
+                <form action="insert.php" method="POST">
+                    Firstname : <br>
+                    <input type="text" name="firstname" value="<?php echo $firstname?>"><br><br>
+
+                    Lastname : <br>
+                    <input type="text" name="lastname" value="<?php echo $lastname?>"><br><br>
+
+                    Email : <br>
+                    <input type="email" name="email" value="<?php echo $email?>"><br><br>
+                    <input type="text" name="email" value="<?php echo $id?>" hidden><br><br>
+
+                    <input type="submit" value="insert" name="edit" class="btn btn-success">
+                </form>
+            </div>
+            <div class="col-sm-3"></div>
+        </div>
+    </div>
+</body>
+</html>
